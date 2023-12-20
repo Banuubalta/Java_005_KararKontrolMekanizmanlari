e8140ebf94c9da4051eb425ac6d3a598feab779a
Banu balta <nbanu.balta@gmail.com>
2023-12-20 17:29:57 +0300

    Initial commit

---
+++ src/main/java/com/banubalta/AppMain.java
@@ -0,0 +1,32 @@
+package com.banubalta;
+
+public class AppMain {
+    public static void main(String[] args) {
+
+        System.out.println(" ------------ İF --------");
+          //    ŞART
+        if (1<50){
+            System.out.println("Evet1");
+        }
+
+        int sayi =10;
+        if (1<-sayi){
+            System.out.println("Evet2");
+        }
+
+        int a= 1, b=2 , c=3;
+        int x,y,z;
+
+        x =90;
+        y =50;
+        z=100;
+
+        if ((x+y+z) > (a+ (b*c))){
+            System.out.println("Sağlandi");
+
+
+
+        }
+
+    }
+}
